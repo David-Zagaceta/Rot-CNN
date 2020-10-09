675abdf813e70a2b13433709a3b06c059677ed3d
David-Zagaceta <zagaceta@unlv.nevada.edu>
2020-10-08 19:28:22 -0700

    Increased truncation to 8 to check gpu performance

--- test.py
+++ test.py
@@ -6,13 +6,13 @@ from MLP import MLP
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(device)
 # simulated neighbor list for one atom
-numNeighbors = 50000
+numNeighbors = 500
 nl = torch.rand(numNeighbors,3)
 #nl.requires_grad = True
 # CNN params
-L = 1
-T = 3
-Nconv = 2
+L = 8
+T = 12
+Nconv = 3
 
 # construct a network
 RotCNN = torch.nn.Sequential(SphericalHarmonicTransform(L),
@@ -20,6 +20,8 @@ RotCNN = torch.nn.Sequential(SphericalHarmonicTransform(L),
                             ConvLinear(L,T,1,Nconv),
                             CgTransform(L),
                             ConvLinear(L,T,2,Nconv),
+                            CgTransform(L),
+                            ConvLinear(L,T,3,Nconv),
                             MLP(n_in=T))
 
 x = RotCNN(nl)

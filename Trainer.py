3c60abd1457ae75e4096cddd74a1c885e5d4a633
David-Zagaceta <zagaceta@unlv.nevada.edu>
2020-11-23 13:51:37 -0800

    added logging

--- Trainer.py
+++ Trainer.py
@@ -1,5 +1,6 @@
 import torch
 import numpy as np
+import logging
 
 class Trainer:
 
@@ -12,8 +13,8 @@ class Trainer:
         self.validationinterval = validationinterval
 
     def train(self, epochs):
-        evals = []
-        print("Training")
+        logging.basicConfig(filename='training.log', filemode='w', level=logging.INFO, format='%(name)s - %(levelname)s - %(message)s')
+        logging.info("Training")
         for _ in range(epochs):
             for nl, eng in self.trainingdata:
                 #self.optimizer.zero_grad()
@@ -33,41 +34,5 @@ class Trainer:
                     for nl, eng in self.validationdata:
                         eng_pred = self.model.forward(nl)
                         score += abs((eng-eng_pred).sum())/nl.shape[0]
-                    print(score)
-                    evals.append(score)
+                    logging.info(score)
                     self.model = self.model.train()
-        if self.validationdata != None:
-            evals = np.array(evals)
-            np.save('training.npy', evals)
-
-    def __name__(self):
-        return 'why'
-
-def train(epochs, model, optimizer, lossfn, trainingdata, validationdata=None, validationinterval=1):
-    evals = []
-    print("Training")
-    for _ in range(epochs):
-        for nl, eng in self.trainingdata:
-            #self.optimizer.zero_grad()
-            for param in self.model.parameters():
-                param.grad = None
-            eng_pred = self.model.forward(nl)
-            loss = self.lossfn(eng_pred, eng)/nl.shape[0]
-            loss.backward()
-            self.optimizer.step()
-
-        if self.validationdata == None:
-            pass
-        else:
-            if _ % self.validationinterval == 0:
-                score = 0
-                self.model = self.model.eval()
-                for nl, eng in self.validationdata:
-                    eng_pred = self.model.forward(nl)
-                    score += abs((eng-eng_pred).sum())/nl.shape[0]
-                print(score)
-                evals.append(score)
-                self.model = self.model.train()
-    if self.validationdata != None:
-        evals = np.array(evals)
-        np.save('training.npy', evals)
--- test.py
+++ test.py
@@ -11,12 +11,10 @@ from torch.utils.data import DataLoader
 from Trainer import Trainer
 import time
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-#device = 'cpu'
-
+#device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+device = 'cpu'
 TrainData = "training.json"
 TestData  = "test.json"
-
 trainingset = parse_json(TrainData)
 validationset = parse_json(TestData)
 
@@ -24,7 +22,7 @@ validationset = parse_json(TestData)
 rcut = 4.9
 
 # CNN params
-L = 8
+L = 4
 T = 12
 Nconv = 3
 
@@ -34,15 +32,15 @@ nlcalc = AseEnvironmentProvider(rcut)
 # with only energy training
 trainingdata = SimpleAtomsDataset(trainingset, nlcalc, device=device)
 validationdata = SimpleAtomsDataset(validationset, nlcalc, device=device)
-
 cgn = CGnet(L,T,Nconv,rcut,device=device,skip=0)
 cgn = cgn.to(device)
-
 
 criterion = torch.nn.MSELoss()
 # create optimizer
 optimizer = optim.Adam(cgn.parameters(), lr=3e-4)
 
-nepochs = 1000
+nepochs = 250
 
-Trainer = Trainer(cgn, optimizer, criterion, trainingdata, trainingdata, validationinterval=20)
+Trainer = Trainer(cgn, optimizer, criterion, trainingdata, validationdata, validationinterval=20)
+
+Trainer.train(nepochs)
